07c842674fc6a0b236cb5f4898866eda7c5b3981
duonggiakhanhb <duonggiakhanhb@gmail.com>
2021-09-05 17:50:01 +0700

    update data

--- screens/Home.js
+++ screens/Home.js
@@ -2,40 +2,34 @@ import React from "react";
 import { StyleSheet, Dimensions, ScrollView } from "react-native";
 import { Block, theme } from "galio-framework";
 
-import { Card } from "../components";
+import { Card, Input } from "../components";
 import articles from "../constants/articles";
 const { width } = Dimensions.get("screen");
 
 class Home extends React.Component {
-    renderArticles = () => {
+    RegisterHCMUE = () => {
         return (
-            <ScrollView
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={styles.articles}
-            >
-                <Block flex>
-                    <Card item={articles[0]} horizontal />
-                    <Block flex row>
-                        <Card
-                            item={articles[1]}
-                            style={{ marginRight: theme.SIZES.BASE }}
-                        />
-                        <Card item={articles[2]} />
-                    </Block>
-                    <Card item={articles[3]} horizontal />
-                    <Card item={articles[4]} full />
-                </Block>
-            </ScrollView>
-        );
-    };
-
-    render() {
-        return (
-            <Block flex center style={styles.home}>
+            <Block style={{ paddingHorizontal: theme.SIZES.BASE }}>
+                <Input
+                right
+                placeholder="Icon Right"
+                iconContent={
+                    <Icon
+                    size={11}
+                    color={argonTheme.COLORS.ICON}
+                    name="search-zoom-in"
+                    family="ArgonExtra"
+                    />
+                }
+                />
+            </Block>
+        )
+    }
+    render (
+        <Block flex center style={styles.home}>
                 {this.renderArticles()}
             </Block>
-        );
-    }
+    )
 }
 
 const styles = StyleSheet.create({
@@ -49,3 +43,9 @@ const styles = StyleSheet.create({
 });
 
 export default Home;
+
+// v9lFjxSUefXHFumlk3+50ft5gBiiiD/iUHyBy4e6+a6DZSeRWJra6P7UYET1r8Y+yzqME8PzQD4=         2111COMP104601
+// v9lFjxSUefXHFumlk3 50ft5gBiiiD/iUHyBy4e6 a6DZSeRWJra6P7UYET1r8Y yzqME8PzQD4=|
+// v9lFjxSUefVMiknmyxXmoQX1iSPof o71iXbQ4RYx4Q8LtMtkA2P38w9SJb1oq9gpPDo9IGK9yI=|    header 2111COMP131501
+// v9lFjxSUefVMiknmyxXmoQX1iSPof+o71iXbQ4RYx4Q8LtMtkA2P38w9SJb1oq9gpPDo9IGK9yI=     value in dom
+// MjExMUNPTVAxMDQ2MDEkMC4wJDIxMTFDT01QMTA0NjAxJCQw
